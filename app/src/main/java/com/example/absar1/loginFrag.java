87b6c95e48c1b50737c2ab76dfde9526eb87af6c
Khaled Abed <khaledabed2601@gmail.com>
2022-11-04 14:25:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/absar1/loginFrag.java
@@ -0,0 +1,88 @@
+package com.example.absar1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link loginFrag#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class loginFrag extends Fragment {
+    private EditText etUsername,etPassword;
+    private Button btnLogin;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public loginFrag() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment loginFrag.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static loginFrag newInstance(String param1, String param2) {
+        loginFrag fragment = new loginFrag();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        connectComponents();
+    }
+
+    private void connectComponents() {
+        etUsername=getView().findViewById(R.id.etUserFL);
+        etPassword=getView().findViewById(R.id.etPassFL);
+        btnLogin=getView().findViewById(R.id.btnLogFL);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               String username=etUsername.getText().toString();
+               String password=etPassword.getText().toString();
+
+            }
+        });
+    }
+}
